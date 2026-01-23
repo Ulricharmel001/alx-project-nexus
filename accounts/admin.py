d6f2e546000d42815fcc5bea265efee2a143c3d3
Ulrich Armel <armelulrich16@gmail.com>
2026-01-23 12:54:46 +0100

    feat(auth): custom user model

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
 
+from .models import CustomUser
+
+
 # Register your models here.
+# customizing the admin interface for CustomUser model
+@admin.register(CustomUser)
+class CustomUserAdmin(admin.ModelAdmin):
+    list_display = ("username", "email", "is_staff", "is_active")
+    search_fields = ("username", "email")
+    list_filter = ("is_staff", "is_active")
+    ordering = "username"
