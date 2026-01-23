04ce7103a6a0c12b03649d41d7963518a24e8144
Ulrich Armel <armelulrich16@gmail.com>
2026-01-23 08:51:10 +0100

    feat(swagger): setup the project for proper API documentation with swagger

--- e_commerce_api/urls.py
+++ e_commerce_api/urls.py
@@ -1,23 +1,33 @@
 """
 URL configuration for e_commerce_api project.
 
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/5.2/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+The `urlpatterns` list routes URLs to views.
 """
+
+from django.conf import settings
 from django.contrib import admin
 from django.urls import include, path
+from drf_yasg import openapi
+from drf_yasg.views import get_schema_view
+from rest_framework import permissions
+
+schema_view = get_schema_view(
+    openapi.Info(
+        title="E-Commerce API",
+        default_version="v1",
+        description="API documentation for authentication and services",
+        contact=openapi.Contact(email=settings.SUPPORT_EMAIL),
+    ),
+    public=True,
+    permission_classes=(permissions.AllowAny,),
+)
 
 urlpatterns = [
+    # Admin
     path("admin/", admin.site.urls),
-    path("api/accounts/", include("accounts.urls")),
+    # API routes
+    path("api/v1/accounts/", include("accounts.urls")),
+    # API Documentation
+    path("swagger/", schema_view.with_ui("swagger", cache_timeout=0)),
+    path("redoc/", schema_view.with_ui("redoc", cache_timeout=0)),
 ]
