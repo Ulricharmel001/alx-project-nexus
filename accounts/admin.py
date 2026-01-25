40e2befa0ec522caf6cfa40a16373e14d4d2716e
Ulrich Armel <armelulrich16@gmail.com>
2026-01-25 14:29:24 +0100

    fix(remove-project): remoove a project i mistaken start instead of app

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,5 +1,8 @@
 from django.contrib import admin
+
 from .models import CustomUser, UserProfile
+
+
 # Register your models here.
 # customizing the admin interface for CustomUser model
 @admin.register(CustomUser)
@@ -9,8 +12,9 @@ class CustomUserAdmin(admin.ModelAdmin):
     list_filter = ("is_staff", "is_active")
     ordering = ("email",)
 
+
 @admin.register(UserProfile)
 class UserProfileAdmin(admin.ModelAdmin):
-    list_display = ('bio', 'phone_number', 'address')
-    search_fields = ('address', 'phone_number')
-    ordering = ('phone_number',)
+    list_display = ("bio", "phone_number", "address")
+    search_fields = ("address", "phone_number")
+    ordering = ("phone_number",)
